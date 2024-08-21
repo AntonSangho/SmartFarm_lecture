bc9e96cc179cb81b3b6986afc7f1f3b31d37e245
AntonSangho <sanghoemail@gmail.com>
2024-08-21 20:38:05 +0900

    스트립 테스트 코드 추가

--- src/simpletest/Neopixel_strip.py
+++ src/simpletest/Neopixel_strip.py
@@ -13,27 +13,59 @@
 from machine import Pin
 import time
 from neopixel import NeoPixel
+# 환풍기 핀 초기화
+led = Pin('LED', Pin.OUT)
+button = Pin(20, Pin.IN, Pin.PULL_UP)
+
+# 버튼 상태를 추적하는 변수 초기화
+button_state = False
+
+# 버튼이 눌렸을 때 호출될 핸들러 함수 정의
+def button_handler(pin):
+    global button_state
+    # 버튼 상태 전환
+    button_state = not button_state
+    if button_state == True:
+        np1_on()
+        np2_on()
+    else:
+        np1_off()
+        np2_off()
+    
+# 버튼에 핸들러 등록
+button.irq(trigger=Pin.IRQ_FALLING, handler=button_handler)
 
 # 네오픽셀 핀 초기화
-np0 = NeoPixel(machine.Pin(13), 30)
+np1 = NeoPixel(machine.Pin(13), 30)
+np2 = NeoPixel(machine.Pin(14), 30)
 
 # 네오픽셀 상태를 추적하는 변수 초기화 
-def np_on():
-    for i in range(0, np0.n):
-        np0[i] = (255,0,0)
-    np0.write()
-def np_off():
-    for i in range(0, np0.n):
-        np0[i] = (0,0,0)
-    np0.write()
+def np1_on():
+    for i in range(0, np1.n):
+        np1[i] = (255,0,0)
+    np1.write()
+def np1_off():
+    for i in range(0, np1.n):
+        np1[i] = (0,0,0)
+    np1.write()
+
+def np2_on():
+    for i in range(0, np2.n):
+        np2[i] = (0,255,0)
+    np2.write()
+def np2_off():
+    for i in range(0, np2.n):
+        np2[i] = (0,0,0)
+    np2.write()
+
 
 while True:
     # 네오픽셀 켜기
-    #np_on()
-    #time.sleep(1)
-    # 네오픽셀 끄기
-    np_off()
+    #np1_on()
+    #np2_on()
     time.sleep(1)
-
-
+    # 네오픽셀 끄기
+    #np1_off()
+    #np2_off()
+    #time.sleep(1)
 
