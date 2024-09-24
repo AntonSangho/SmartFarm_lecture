d5bd9ee327d0ae83b5a77c98e0f766085d7771ce
AntonSangho <sanghoemail@gmail.com>
2024-09-24 09:22:49 +0900

    프로젝트용 파일main.py 추가

--- src/main.py
+++ src/main.py
@@ -1,150 +1,170 @@
-import machine
-import network
-import time
+"""
+DataPi_v1.py 기능
+- 버튼을 누르면 온습도, 가스 센서 데이터를 OLED에 표시하고, 파일에 기록합니다.
+- 기록 중일 때는 네오픽셀이 켜지고, 기록 중지 상태에서는 네오픽셀이 꺼집니다.
+- 기록 중일 때는 OLED에 "Recording"이라고 표시하고, 기록 중지 상태에서는 "Stop"이라고 표시합니다.
+- 기록 중일 때는 온습도, 가스 센서 데이터를 OLED에 표시하고, 파일에 기록합니다.
+- 파일에는 "날짜, 시간, 온도, 습도, AQI, ECO2, TVOC" 데이터를 기록합니다.
+"""
+import random
+from machine import Pin, SoftI2C, PWM 
+import utime
+from neopixel import NeoPixel
+import ssd1306
+from machine import RTC
+from ds3231_port import DS3231
 import AHT21
 import ENS160
-import settings
-import urequests
 
-# boot pattern
-led = machine.Pin("LED", machine.Pin.OUT)
-print("Playing LED boot pattern...")
-led.on()
-time.sleep(0.5)
-led.off()
-time.sleep(0.5)
-led.on()
-time.sleep(0.5)
-led.off()
-time.sleep(0.5)
-led.on()
-time.sleep(0.5)
-led.off()
-time.sleep(0.5)
+# 상태변수 
+sensging_active = False
+recording_active = False
+recording_interval = 10   
+file = None
 
-# set up
-print("Setting up ENS160 and AHT21 interface via I2C...")
-i2c = machine.I2C(1, sda=machine.Pin(14), scl=machine.Pin(15))
-aht = AHT21.AHT21(i2c)
-ens = ENS160.ENS160(i2c)
+# LED, 버튼, 부저 설정
+Led = Pin("LED", Pin.OUT)
+button = Pin(20, Pin.IN, Pin.PULL_UP)
+buzzer = PWM(Pin(22))
+
+# RTC I2C 포트를 설정합니다.
+i2c0 = machine.I2C(0, scl=machine.Pin(5), sda=machine.Pin(4), freq=400000)
+rtc = DS3231(i2c0)
+
+# OLED 픽셀 크기 설정
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c0)
+
+# AHT21 I2C 포트를 설정합니다.
+i2c1 = machine.I2C(1,scl=machine.Pin(15), sda=machine.Pin(14),freq=400000)
+aht = AHT21.AHT21(i2c1)
+
+# 가스 센서 I2C 포트를 설정합니다. 
+ens = ENS160.ENS160(i2c1)
 ens.reset()
-time.sleep(0.5)
+utime.sleep(0.5)
 ens.operating_mode = 2
-time.sleep(2.0)
+#utime.sleep(2.0)
 
-# connect to wifi
-print("Preparing for wifi connection...")
-wifi_con_attempt:int = 0
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-while wlan.isconnected() == False:
+# 파일 생성 
+f = open('data.csv', 'a')
 
-    wifi_con_attempt = wifi_con_attempt + 1
-
-    # blip light
-    led.on()
-    time.sleep(0.1)
-    led.off()
-    
-    print("Attempt #" + str(wifi_con_attempt) + " to connect to wifi...")
-    wlan.connect(settings.ssid, settings.password)
-    time.sleep(3)
-print("Connected to wifi after " + str(wifi_con_attempt) + " tries!")
-my_ip:str = str(wlan.ifconfig()[0])
-print("My IP Address: " + my_ip)
-
-# create watchdog timer
-wdt = machine.WDT(timeout=8388) # 8,388 ms is the limit (8.388 seconds)
-wdt.feed()
-print("Watchdog timer now activated.")
-
-# Enter infinite loop
-samples_uploaded:int = 0
-while True:
-
-    # LED on while we are doing something
-    led.on()
-
-    # take reading from ENS160
-    print("Taking ENS160 measurements... ")
+def record_data():
+    # 온습도 센서에서 데이터 읽기
+    rht = aht.read()
+    humidity = rht[0]
+    temperature = rht[1]
+    # 가스 센서에서 데이터 읽기
     aqi:int = ens.AQI
     eco2:int = ens.CO2
     tvoc:int = ens.TVOC
-    print("AQI: " + str(aqi) + ", ECO2: " + str(eco2) + ", TVOC: " + str(tvoc))
-    wdt.feed()
 
-    # if there was an error getting legit values from the ENS160
-    if aqi == 0 or eco2 == 0 or tvoc == 0:
-        print("AQI, ECO2, or TVOC readings were not successful! Going into troubleshooting mode.")
-        while aqi == 0 or eco2 == 0 or tvoc == 0: # go into troubleshooting mode, trying to recover ENS160 functionality, until solved for.
+    now = rtc.get_time()
+    print("Time: {}/{} {}:{}:{}".format(now[1], now[2], now[3], now[4], now[5]))
+    print("Humidity: {:.2f}%".format(humidity))
+    print("Temperature: {:.2f}C".format(temperature))
+    print("AQI: {}, ECO2: {}, TVOC: {}".format(aqi, eco2, tvoc))
+    oled.fill(0)
+    oled.text("[TeamName]", 0, 0)
+    oled.text("Time: {}:{}:{}".format(now[3], now[4], now[5]), 0, 10)
+    oled.text("Temp: {:.2f}C".format(temperature), 0, 20)
+    oled.text("Humid: {:.2f}%".format(humidity), 0, 30)
+    oled.text("ECO2: {}".format(eco2), 0, 40)
+    oled.text("TVOC: {}".format(tvoc), 0, 50)
 
-            # print msg
-            print("AQI/ECO2/TVOC reading unsuccessful at last attempt. Will try to reset again.")
+    oled.show() 
+    # 파일에 데이터 기록
+    f.write("{}/{} {}:{}:{},{:.2f},{:.2f},{},{},{}\n".format(now[1], now[2], now[3], now[4], now[5], humidity, temperature, aqi, eco2, tvoc))
 
-            # flash quickly to show an issue
-            print("Playing troubleshooting LED pattern...")
-            for x in range(0, 5): 
-                led.on()
-                time.sleep(0.05)
-                led.off()
-                time.sleep(0.05)
-                wdt.feed()
-            
-            print("Resetting ENS160...")
-            ens.reset() # reset
-            wdt.feed() # feed watchdog timer
-            
-            # take sample
-            print("Sampling ENS160 after reset...")
-            time.sleep(1)
-            led.on()
-            aqi = ens.AQI
-            eco2 = ens.CO2
-            tvoc = ens.TVOC
-            wdt.feed()
-            led.off()
-    
-    # take reading from AHT21
-    print("Taking AHT21 measurements...")
-    rht = aht.read()
-    humidity:float = rht[0]
-    humidity = humidity / 100
-    temperature:float = rht[1]
-    wdt.feed()
+    utime.sleep(1)  
 
-    # create json body
-    body = {"aqi": aqi, "eco2": eco2, "tvoc": tvoc, "humidity": humidity, "temperature": temperature}
-    print("Measurements taken! " + str(body))
-    wdt.feed()
-    
-    # make HTTP call
-    print("Making HTTP call...")
-    wdt.feed()
-    pr = urequests.post(settings.post_url, json=body)
-    wdt.feed()
-    pr.close()
-    print("HTTP call made!")
+# 부저를 울리는 함수
+def button_buzzer(freq):
+    buzzer.duty_u16(30000)
+    buzzer.freq(freq)
+    utime.sleep(0.1)
+    buzzer.duty_u16(0)
 
-    # if the status code of the HTTP response was not succesful (not in the 200 range), go into an infinite loop of
-    # This is here so the program will stop if, for example, the receiving endpoint is no longer active or accepting.
-    if str(pr.status_code)[0:1] != "2":
-        while True: 
-            led.on()
-            wdt.feed()
-            time.sleep(1)
-            led.off()
-            wdt.feed()
-            time.sleep(1)
+# 부팅을 알리는 부저 소리
+def start_buzzer():
+    buzzer.freq(1000)
+    buzzer.duty_u16(30000)
+    utime.sleep(0.1)
+    buzzer.freq(2000)
+    buzzer.duty_u16(30000)
+    utime.sleep(0.1)
+    buzzer.freq(3000)
+    buzzer.duty_u16(30000)
+    utime.sleep(0.1)
+    buzzer.duty_u16(0)
+
+
+# 버튼의 상태를 추적하는 변수 초기화    
+button_state = False
+countdown_start_time = None 
+
+# 버튼이 눌렸을 때 호출될 핸들러 함수 정의
+def button_handler(pin):
+    global recording_active, file
+    if pin.value() == 0:  # 버튼이 눌렸을 때
+        recording_active = not recording_active
+        if recording_active:
+            print("기록을 시작합니다.")
+            oled.fill(0)
+            oled.text("Recording", 0, 0)
+            oled.show()
+            Led.value(1)
+            np_on()
+            button_buzzer(2000)
+            file = open('data.csv', 'a')
+        else:
+            print("기록을 중지합니다.")
+            oled.fill(0)
+            oled.text("Stop", 0, 0)
+            oled.show()
+            np_off()
+            button_buzzer(2000)
+            if file:
+                Led.value(0)
+                file.close()
+                np_off()
+    global button_state, countdown_start_time
+    # 버튼 상태 전환
+    button_state = not button_state
+    if button_state == True:
+        countdown_start_time = utime.ticks_ms()
+        np_on()
+        record_data()
     else:
-        print("Sample upload successfully accepted!")
+        np_off()
+    
+# 버튼에 핸들러 등록
+button.irq(trigger=Pin.IRQ_FALLING, handler=button_handler)
 
-    # increment tracker
-    samples_uploaded = samples_uploaded + 1
+# 네오픽셀 핀 초기화
+np = NeoPixel(machine.Pin(21), 1)
 
-    # wait for time
-    led.off() # led off while doing nothing (just waiting)
-    next_loop:int = time.ticks_ms() + (1000 * settings.sample_time_seconds)
-    while (time.ticks_ms() < next_loop):
-        print("Sampling #" + str(samples_uploaded + 1) + " next in " + str(round((next_loop - time.ticks_ms()) / 1000, 0)) + " seconds...")
-        time.sleep(1)
-        wdt.feed()
+# 네오픽셀 상태를 추적하는 변수 초기화 
+def np_on():
+    for i in range(0, np.n):
+        np[i] = (70,50,20)
+    np.write()
+def np_off():
+    for i in range(0, np.n):
+        np[i] = (0,0,0)
+    np.write()
+
+# ds3231으로 부터 시간을 가져오는 함수 정의
+def get_time():
+    rtc = RTC()
+    return rtc.datetime()
+
+start_buzzer()
+while True:
+    if recording_active:
+        record_data()
+        utime.sleep(recording_interval)
+    else:
+        Led.value(0)
+    
